ec496b7655fc11fa1e8ff907d65c613dcc55a606
Ben Foreman <benf.drig@gmail.com>
2026-01-24 11:41:09 -0500

    feat: Add HTMLNode with tests

---
+++ src/htmlnode.py
@@ -0,0 +1,30 @@
+class HTMLNode():
+    def __init__(self, tag=None, value=None, children=None, props=None):
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props
+
+    def to_html(self):
+        raise NotImplementedError
+
+    def props_to_html(self):
+        if self.props is None or len(self.props) == 0:
+            return ""
+        atts = []
+        for k, v in self.props.items():
+            atts.append(f'{k}="{v}"')
+        return " " + " ".join(atts)
+
+    def __eq__(self, other):
+        if self.tag != other.tag or self.value != other.value or self.props != other.props:
+            return False
+        if self.children is not None:
+            for i in range(self.children):
+                if self.children[i] != other.children[i]:
+                    return False
+        return True
+
+    def __repr__(self):
+        return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
+
---
+++ src/test_htmlnode.py
@@ -0,0 +1,40 @@
+import unittest
+
+from htmlnode import HTMLNode
+
+
+class TestHTMLNode(unittest.TestCase):
+    def test_eq(self):
+        node = HTMLNode("p", "Hello world!")
+        # node2 = HTMLNode("p", "Hello world!")
+        node2 = HTMLNode("p", "Hello world!")
+        self.assertEqual(node, node2)
+    def test_tag_not_eq(self):
+        node = HTMLNode("p", "Hello world!")
+        node2 = HTMLNode("div", "Hello world!")
+        self.assertNotEqual(node, node2)
+    def test_value_not_eq(self):
+        node = HTMLNode("p", "Hello world!")
+        node2 = HTMLNode("p", "Hello universe!")
+        self.assertNotEqual(node, node2)
+    def test_props_not_eq(self):
+        node = HTMLNode("a", "Hello world!", props={"href": "https://selevas.com"})
+        node2 = HTMLNode("a", "Hello world!", props={"href": "https://google.com"})
+        node3 = HTMLNode("a", "Hello world!", props={"href": "https://selevas.com", "target": "_blank"})
+        self.assertNotEqual(node, node2)
+        self.assertNotEqual(node, node3)
+        self.assertNotEqual(node2, node3)
+    def test_to_html(self):
+        node = HTMLNode("a", "Hello world!")
+        node2 = HTMLNode("a", "Hello world!", props={"href": "https://selevas.com"})
+        node3 = HTMLNode("a", "Hello world!", props={"href": "https://selevas.com", "target": "_blank"})
+        print(node.props_to_html())
+        print(node2.props_to_html())
+        print(node3.props_to_html())
+        self.assertEqual(node.props_to_html(), "")
+        self.assertEqual(node2.props_to_html(), ' href="https://selevas.com"')
+        self.assertEqual(node3.props_to_html(), ' href="https://selevas.com" target="_blank"')
+
+
+if __name__ == "__main__":
+    unittest.main()
