55eec9aac390dd768a34a296f4ac9fba0df32fcf
Benjamin Foreman <benjamin.t.foreman@gmail.com>
2026-01-27 21:39:23 -0500

    fix: Change TextType.PLAIN to TEXT and add CODE

--- src/textnode.py
+++ src/textnode.py
@@ -1,9 +1,10 @@
 from enum import Enum
 
 class TextType(Enum):
-    PLAIN = "plain"
+    TEXT = "text"
     BOLD = "bold"
     ITALIC = "italic"
+    CODE = "code"
     LINK = "link"
     IMAGE = "image"
 
